a24a37e977851e68e8af717114fc12a8a5857fae
Langegger, Lukas <langegger@walter-group.com>
2023-01-05 12:56:15 +0100

    add forecasts, some minor refactoring

--- airbyte-integrations/connectors/source-transporeon-insights/source_transporeon_insights/source.py
+++ airbyte-integrations/connectors/source-transporeon-insights/source_transporeon_insights/source.py
@@ -9,20 +9,6 @@ from airbyte_cdk.sources.streams.http.requests_native_auth import TokenAuthentic
 
 from .streams import *
 
-"""
-TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
-
-This file provides a stubbed example of how to use the Airbyte CDK to develop both a source connector which supports full refresh or and an
-incremental syncs from an HTTP API.
-
-The various TODOs are both implementation hints and steps - fulfilling all the TODOs should be sufficient to implement one basic and one incremental
-stream from a source. This pattern is the same one used by Airbyte internally to implement connectors.
-
-The approach here is not authoritative, and devs are free to use their own judgement.
-
-There are additional required TODOs in the files within the integration_tests folder and the spec.yaml file.
-"""
-
 
 # Source
 class SourceTransporeonInsights(AbstractSource):
@@ -51,4 +37,7 @@ class SourceTransporeonInsights(AbstractSource):
                 SpotPrice(authenticator=auth, config=config),
                 SpotPriceIndex(authenticator=auth, config=config),
                 TotalPriceIndex(authenticator=auth, config=config),
+                SpotPriceForecast(authenticator=auth, config=config),
+                CostIndexForecast(authenticator=auth, config=config),
+                CostIndexFactorsForecast(authenticator=auth, config=config),
                 ]
